c02d52716c74a7f0e662ff637a678a207322bc5d
ChristBernardS <117885718+ChristBernardS@users.noreply.github.com>
2022-11-25 17:04:41 +0700

    Delete 2_B_71220833.py

--- 2_B_71220833.py
+++
@@ -1,6 +0,0 @@
-print('========== INI PROGRAM SEDER HANA ==========')
-print('Masukan Angka yang ingin di Pangkatkan:')
-Angka = int(input('>> '))
-Pangkat7 = Angka**7
-print(" ")
-print('Angka anda telah di pangkat kan 7! anda mendapat angka : ',Pangkat7)
