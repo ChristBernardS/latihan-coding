6143c3e15cbe980b21ed3a9093e8eb451c283365
ChristBernardS <117885718+ChristBernardS@users.noreply.github.com>
2022-11-25 17:04:29 +0700

    Delete 1_B_71220833.py

--- 1_B_71220833.py
+++
@@ -1,4 +0,0 @@
-print('========== ALAT PENGHITUNG LINGKARAN SENTOT ==========')
-Diameter = int(input('Masukan Diameter (Meter) : '))
-KelilingLingkaran = round(2*22/7*1/2*Diameter, ndigits=None)
-print('Jarak yang harus di tempuh : ',KelilingLingkaran,' Meter')
